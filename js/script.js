18641468cd8e79c0dcb5010a9273e0aeb89be598
Veronika <veronikakuchtova95@gmail.com>
2020-11-23 14:48:43 +0100

    M2 Change Avatar and name on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 var app = new Vue ({
   el: '#panel',
-  data:
+  data: {
+    i: [0],
+    avatars: [
+      'img/avatar_5.jpg',
+      'img/avatar_2.jpg',
+      'img/avatar_8.jpg',
+      'img/avatar_4.jpg'
+    ],
+    names: [
+      'Link',
+      'Kitbooga',
+      'Hipster Mike',
+      'Lewis'
+    ]
+  },
+  methods: {
+    openConv: function (index) {
+      this.i = index;
+    }
+
+
+  }
 
 
 
