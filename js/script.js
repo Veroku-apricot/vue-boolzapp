2a447b63e181bc017d77e7dca799b3b8cbe49b7e
Veronika <veronikakuchtova95@gmail.com>
2020-11-27 10:34:32 +0100

    Added date and time

--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@ var app = new Vue ({
       user: true,
       chat: [{
         message: 'Hey i found a weird green cape... is it yours?',
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.30'
       },
       {
         message: "Nope, don't think so",
-        status: 'sent'
+        status: 'sent',
+        time: '20.11.2020 11.32'
       }]
     },
       // Conversation 2
@@ -25,15 +27,18 @@ var app = new Vue ({
       user: true,
       chat: [{
         message: 'Hey there, how are you? Hope you doing fine!',
-        status: 'sent'
+        status: 'sent',
+        time: '20.11.2020 11.30'
       },
       {
         message: "Hello!",
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.32'
       },
       {
         message: "Yup, all good, thanks!",
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.32'
       }]
     },
       // Conversation 3
@@ -43,15 +48,18 @@ var app = new Vue ({
       user: true,
       chat: [{
         message: 'Come over and check out my beard',
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.30'
       },
       {
         message: "Ehm... No thanks",
-        status: 'sent'
+        status: 'sent',
+        time: '20.11.2020 11.34'
       },
       {
         message: "Envious...",
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.36'
       }]
     },
       // Conversation 4
@@ -61,15 +69,18 @@ var app = new Vue ({
       user: true,
       chat: [{
         message: "Hey, wanna hang out at the park? I'm with my dog",
-        status: 'sent'
+        status: 'sent',
+        time: '20.11.2020 11.30'
       },
       {
         message: "Sure! My cousin Luisa is coming too :)",
-        status: 'received'
+        status: 'received',
+        time: '20.11.2020 11.31'
       },
       {
         message: "Nice!",
-        status: 'sent'
+        status: 'sent',
+        time: '20.11.2020 11.33'
       }]
     }]
   },
@@ -82,22 +93,28 @@ var app = new Vue ({
     send: function () {
       if (this.newText === ''){
       }else{
-        this.conversations[this.i].chat.push({message: this.newText, status: 'sent'});
+        this.conversations[this.i].chat.push({message: this.newText, status: 'sent', time: 'Oggi, ' + this.getTime()});
         this.newText = '';
         // Receive message
         setTimeout (() => {
-          this.conversations[this.i].chat.push({message: 'Ok dude', status: 'received'});
+          this.conversations[this.i].chat.push({message: 'Ok dude', status: 'received', time: 'Oggi, ' + this.getTime()});
         }, 1500)
       }
     },
     filter: function () {
-      this.conversations.forEach(user => {
-        if (user.name.toLowerCase().includes(this.searchContact.toLowerCase())){
-          user.user= true;
+      this.conversations.forEach(obj => {
+        if (obj.name.toLowerCase().includes(this.searchContact.toLowerCase())){
+          obj.user= true;
         }else{
-          user.user= false;
+          obj.user= false;
         }
       })
+    },
+    getTime: function () {
+      var d = new Date();
+      var hour = d.getHours();
+      var minutes = d.getMinutes();
+      return hour + ':' + minutes;
     }
   }
 
